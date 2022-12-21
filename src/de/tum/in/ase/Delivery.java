bb4222a89d18e8d1576154194a13607fb55ac548
Wan-Ting <ge26quw@mytum.de>
2022-12-21 02:21:44 +0100

    try 4

--- src/de/tum/in/ase/Delivery.java
+++ src/de/tum/in/ase/Delivery.java
@@ -1,9 +1,11 @@
 package de.tum.in.ase;
+import java.util.stream.*;
+import java.util.stream.Collectors;
 
 import java.util.*;
 
 // TODO: implement interface
-public class Delivery<E> implements Iterable<E> {
+public class Delivery implements Iterable<Package> {
 
     private final String address;
     private final Map<String, Set<Package>> packagesByAddress;
@@ -18,55 +20,19 @@ public class Delivery<E> implements Iterable<E> {
         this.packagesByAddress = new HashMap<>();
     }
 
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>(){
-            private Set address= packagesByAddress.keySet();
-            private List<Set<Package>> packageList = new ArrayList<Set<Package>>();
-            private List<Map<String, Set<Package>>>addressList= new ArrayList<Map<String, Set<Package>>>();
-            private int position=0;
-
-            private void addPackList(){
-
-            }
-            @Override
-            public boolean hasNext() {
-                return position<packagesByAddress.size();
-            }
-
-            @Override
-            public E next() {
-                if(position==packagesByAddress.size()){
-                    throw new NoSuchElementException();
-                }
-
-                return null;
-            }
-        };
-    }
-
-
     public String getAddress() {
         return address;
     }
 
     public void add(Package aPackage) {
         // TODO implement method
-        Set<Package> p = new HashSet<>();
         if (!packagesByAddress.containsKey(aPackage.getAddress())) {
+            Set<Package> p = new HashSet<>();
             p.add(aPackage);
-            packagesByAddress.put(aPackage.getAddress(), p);
+            packagesByAddress.put(aPackage.getAddress(),p);
         } else {
-            p.add(aPackage);
             packagesByAddress.get(aPackage.getAddress()).add(aPackage);
         }
-        //System.out.println(packagesByAddress);
-        System.out.println("how many address: "+packagesByAddress.size());
-        System.out.println("each packages detail: "+p);
-        System.out.println("The key for each address: "+packagesByAddress.keySet());
-        System.out.println("The value for each address: "+packagesByAddress.values());
-
-        System.out.println();
     }
 
     @Override
@@ -75,37 +41,91 @@ public class Delivery<E> implements Iterable<E> {
     }
 
     // TODO: implement iterator
+    @Override
+    public Iterator<Package> iterator() {
+        return new Iterator<Package>(){
+            private List<String> sortedKeys = new ArrayList<String>();
+            private List<Set<Package>>value= new ArrayList<>();
+            private List<Package> eachP= new ArrayList<>();
+            private List<Set<Package>> packageList = new ArrayList<Set<Package>>();
+            private List<Map<String, Set<Package>>>addressKey= new ArrayList<>();
+
+            //private List<String>addressKey= new ArrayList<String>();
+            private int position=0;
+
+            private void sortbykey(){
+                HashMap<String, Set<Package>> tmp= packagesByAddress.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e1, LinkedHashMap::new));
+                sortedKeys = new ArrayList<String>(tmp.keySet());
+                value= new ArrayList<>(tmp.values());
+//                for (int j=0;j< value.size();j++){
+//                    eachP=value.get(j).stream().toList();
+//                }
+                //HashMap<String, Integer> temp= map.entrySet().stream().sorted((i1, i2) -> i1.getKey().compareTo(i2.getKey())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+//                addressKey.add(tmp);
+            }
+            @Override
+            public boolean hasNext() {
+                return position<sortedKeys.size();
+            }
+            @Override
+            public Package next() {
+                sortbykey();
+                if(position==sortedKeys.size()){
+                    throw new NoSuchElementException();
+                }
+                for(int i=0; i<sortedKeys.size();i++){
+                    for (int j=0;j< value.size();j++){
+                        for(Package a:value.get(i)){
+                            return a;
+                        }
+                        //eachP=value.get(j).stream().toList();
+//                        if(position< eachP.size()){
+//                            position++;
+//                            return eachP.get(position);
+////                            Delivery(String address, Map<String, Set<Package>> packages)
+//                        }
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                throw new NoSuchElementException();
+            }
+        };
+    }
 
 
     public static void main(String[] args) {
         // TODO test your code:
-
         Delivery delivery = new Delivery("Deliveryplace 1");
         delivery.add(new Package("Iceavenue 5", "Penguinway 4", 100));
         delivery.add(new Package("Iceavenue 5", "Penguinway 1", 85));
 		delivery.add(new Package("Iceavenue 5", "Penguinway 1", 73));
-//		delivery.add(new Package("Snowlane 3", "Antarcticplace 3", 107));
-//		delivery.add(new Package("Winterhighway 89", "Antarcticplace 27", 20));
-//		delivery.add(new Package("Penguinway 6", "Tierpark Hellabrunn, Tierparkstr. 30", 1));
+		delivery.add(new Package("Snowlane 3", "Antarcticplace 3", 107));
+		delivery.add(new Package("Winterhighway 89", "Antarcticplace 27", 20));
+		delivery.add(new Package("Penguinway 6", "Tierpark Hellabrunn, Tierparkstr. 30", 1));
 //		delivery.add(new Package("Tierpark Hellabrunn, Tierparkstr. 30", "Penguinway 6", 0.3));
 //		delivery.add(new Package("Antarcticplace 123", "Penguroad 1", 6));
+        delivery.iterator().next();
+        Set<Integer>a=new HashSet<>();
+        a.add(1);
+        a.add(2);
+        a.add(2);
+        a.add(3);
+        Set<Integer>b1=new HashSet<>();
+        b1.add(2);
+        System.out.println(a);
+        Map<String, Set<Integer>>b = new HashMap<>();
+        b.put("k1",a);
+        b.put("k2",b1);
 
-//        Iterator<Delivery> it=delivery.iterator();
-//        while (it.hasNext()){
-//            System.out.println(it.next());
-//        }
 
 
-        Set<Integer>num = new HashSet<>();
-        num.add(20);
-        num.add(45);
-        num.add(3);
-        Map<String,Integer> a=new HashMap<>();
-        a.put("one",1);
-        a.put("two",2);
-        a.put("three",3);
-        System.out.println();
-        System.out.println(a.keySet());
-        System.out.println(a.values());
     }
 }
